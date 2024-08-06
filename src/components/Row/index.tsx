e4958ed439aa19a5894d02ab91617b28bda0771f
João Pedro <joaopedron253@gmail.com>
2024-08-06 15:03:46 -0300

    feat: create Cte subcomponent(UI and tests)

--- src/components/Row/index.tsx
+++ src/components/Row/index.tsx
@@ -1,7 +1,9 @@
+import Cte from "./Cte";
 import FreightDate from "./FreightDate";
 import RowRoot from "./RowRoot";
 
 export const Row = {
   Root: RowRoot,
-  FreightDate: FreightDate
+  FreightDate: FreightDate,
+  Cte: Cte
 }
