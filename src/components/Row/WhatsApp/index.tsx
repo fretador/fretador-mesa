aefbdfc157cc35919541ab76178cc94b95b55481
João Pedro <joaopedron253@gmail.com>
2024-08-09 11:13:03 -0300

    create WhatsApp subcomponent with formatting and unit tests

---
+++ src/components/Row/WhatsApp/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from './whatsapp.module.css'
+
+interface WhatsAppProps {
+  whatsApp: string
+}
+
+const WhatsApp = ({whatsApp}: WhatsAppProps) => {
+
+  const formattedWhatsApp = `${whatsApp.slice(0, 2)}-${whatsApp.slice(2, 3)}.${whatsApp.slice(3, 7)}-${whatsApp.slice(7)}`;
+
+  return (
+    <p>{formattedWhatsApp}</p>
+  )
+}
+
+export default WhatsApp;
