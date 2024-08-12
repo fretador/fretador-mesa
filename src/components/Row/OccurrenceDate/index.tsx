27591565c7eec6a96bc9ffc40afea8a02635934f
João Pedro <joaopedron253@gmail.com>
2024-08-12 13:03:35 -0300

    feat: create OccurrenceDate subcomponent (UI and tests)

---
+++ src/components/Row/OccurrenceDate/index.tsx
@@ -0,0 +1,17 @@
+import styles from './occurrenceDate.module.css'
+import React from 'react'
+
+interface OccurrenceDateProps {
+  occurrenceDate: Date;
+}
+
+const OccurrenceDate = ({ occurrenceDate }: OccurrenceDateProps) => {
+
+  const formattedDate = occurrenceDate.toLocaleDateString();
+
+  return (
+    <p className={styles.occurrenceDateValue}>{formattedDate}</p>
+  )
+}
+
+export default OccurrenceDate
