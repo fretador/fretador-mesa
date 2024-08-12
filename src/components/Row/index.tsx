9aa42142a8ad078aa873ac38da62a85a9c573d92
Andrews2kx <andrews2k5@gmail.com>
2024-08-12 12:33:11 -0300

    feat:  add TradeName component and unit test

--- src/components/Row/index.tsx
+++ src/components/Row/index.tsx
@@ -5,16 +5,15 @@ import FreightDate from "./FreightDate";
 import Route from "./Route";
 import RowRoot from "./RowRoot";
 import FreightStatus from "./FreightStatus";
-import OccurrenceType from "./OccurrenceType";
-import CustomerEmail from "./CustomerEmail";
 
 export const Row = {
   Root: RowRoot,
   FreightDate: FreightDate,
   FreightCode: FreightCode,
+  Customer: Customer,
+  TradeName: TradeName,
   Route: Route,
   Cte: Cte,
   Driver: Driver,
-  CustomerEmail: CustomerEmail,
   FreightStatus: FreightStatus,
 };
