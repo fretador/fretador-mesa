b486bdfb750d8b9413a354c6164c79f696e88b37
Rodrigo Alexandre <rodrigo.anst@gmail.com>
2024-08-13 08:18:13 -0300

    feat(sidebar):refatored sidebar to receive coposition pattern

--- src/pages/canva/index.tsx
+++ src/pages/canva/index.tsx
@@ -8,21 +8,97 @@ import styles from "./Canva.module.css";
 import { Row } from "@/components/Row";
 import { useAppSelector } from "@/store/store";
 import { mockBoardUsers } from "@/utils/mocks/mockBoardUsers";
+import {
+  HomeIcon,
+  TruckIcon,
+  PersonAddIcon,
+  PeopleIcon,
+  WarningIcon,
+  FinanceIcon,
+  SettingsIcon,
+  SupportIcon,
+  HelpIcon,
+  LogoutIcon,
+} from "@/utils/icons";
 
 const Canva: React.FC = () => {
   const isRetracted = useAppSelector((state) => state.sidebar.isRetracted);
 
   return (
     <div className={styles.container}>
-      <Header />
       <div className={styles.main}>
-        <SidebarComp.Root user={mockBoardUsers[0]}>
+        <SidebarComp.Root user={mockBoardUsers[0]} className={styles.sidebar}>
           <SidebarComp.Header user={mockBoardUsers[0]} />
+          <SidebarComp.List>
+            <SidebarComp.Item
+              icon={<HomeIcon />}
+              text="HOME"
+              isRetracted={isRetracted}
+              isFocused={true}
+              badge={2}
+            />
+            <SidebarComp.Item
+              icon={<TruckIcon />}
+              text="MEUS FRETES"
+              isRetracted={isRetracted}
+              isFocused={false}
+            />
+            <SidebarComp.Item
+              icon={<PersonAddIcon />}
+              text="MOTORISTAS"
+              isRetracted={isRetracted}
+              isFocused={false}
+            />
+            <SidebarComp.Item
+              icon={<WarningIcon />}
+              text="CLIENTES"
+              isRetracted={isRetracted}
+              isFocused={false}
+            />
+            <SidebarComp.Item
+              icon={<HelpIcon />}
+              text="OCORRÊNCIAS"
+              isRetracted={isRetracted}
+              isFocused={false}
+            />
+            <SidebarComp.Item
+              icon={<FinanceIcon />}
+              text="FINANCEIRO"
+              isRetracted={isRetracted}
+              isFocused={false}
+            />
+            <SidebarComp.Separator isRetracted={isRetracted} />
+            <SidebarComp.Item
+              icon={<SettingsIcon />}
+              text="CONFIGURAÇÕES"
+              isRetracted={isRetracted}
+              isFocused={false}
+            />
+            <SidebarComp.Item
+              icon={<SupportIcon />}
+              text="ATENDIMENTO"
+              isRetracted={isRetracted}
+              isFocused={false}
+            />
+            <SidebarComp.Item
+              icon={<HelpIcon />}
+              text="AJUDA"
+              isRetracted={isRetracted}
+              isFocused={false}
+            />
+            <SidebarComp.Item
+              icon={<LogoutIcon />}
+              text="DESLOGAR"
+              isRetracted={isRetracted}
+              isFocused={false}
+            />
+          </SidebarComp.List>
         </SidebarComp.Root>
+
         <div
-          className={
-            isRetracted ? styles.contentRetracted : styles.contentExpanded
-          }
+          className={`${styles.content} ${
+            isRetracted ? styles.contentRetracted : ""
+          }`}
         >
           <Body>
             <Row.Root>
