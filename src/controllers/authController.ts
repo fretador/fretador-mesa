8c4397cf4213949b7f0afae293a192a423776377
Guilherme Augusto Borges Casagrande <38353118+GuilhermeMAG@users.noreply.github.com>
2024-08-27 14:32:09 -0300

    refactor(AuthController): include handling of profile and profilePicture fields

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,21 +1,32 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { loginSuccess, logout } from "@/store/slices/authSlice";
 import { AuthService } from "@/services/authService";
-import { RootState } from "@/store/store";
 
 export const useAuthController = () => {
 	const dispatch = useDispatch();
-	const isAuthenticated = useSelector(
-		(state: RootState) => state.auth.isAuthenticated
-	);
 
 	const login = async (email: string, password: string) => {
 		try {
-			const { token, name, userEmail } = await AuthService.login(email, password);
-			dispatch(loginSuccess({ token, name, email: userEmail }));
+			const { token, name, userEmail, profile, profilePicture } =
+				await AuthService.login(email, password);
+			dispatch(
+				loginSuccess({ token, name, email: userEmail, profile, profilePicture })
+			);
 		} catch (error) {
 			console.error("Login failed:", error);
 			throw error;
+		}
+	};
+
+	const checkAuthStatus = () => {
+		const token = AuthService.getBoardUserToken();
+		const boardUser = AuthService.getBoardUser();
+
+		if (token && boardUser) {
+			return true;
+		} else {
+			console.log("User is not authenticated");
+			return false;
 		}
 	};
 
@@ -24,24 +35,9 @@ export const useAuthController = () => {
 		dispatch(logout());
 	};
 
-	const checkAuthStatus = () => {
-		const token = AuthService.getToken();
-		const user = AuthService.getUser();
-
-		if (token && user) {
-			dispatch(loginSuccess({ token, name: user.name, email: user.email }));
-			return true;
-		} else {
-			console.log("User is not authenticated");
-			AuthService.logout();
-			return false;
-		}
-	};
-
 	return {
 		login,
-		logoutUser,
-		isAuthenticated,
 		checkAuthStatus,
+		logoutUser,
 	};
 };
