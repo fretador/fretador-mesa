f3b40ea4ac7afc8a4217e07660b71a3b23a199b2
Guilherme Augusto Borges Casagrande <38353118+GuilhermeMAG@users.noreply.github.com>
2024-08-27 14:32:31 -0300

    refactor(authSlice): update state management to include new profile fields

--- src/store/slices/authSlice.ts
+++ src/store/slices/authSlice.ts
@@ -5,6 +5,8 @@ interface AuthState {
 	token: string | null;
 	name: string | null;
 	email: string | null;
+	profile: string | null;
+	profilePicture: string | null;
 }
 
 const initialState: AuthState = {
@@ -12,12 +14,16 @@ const initialState: AuthState = {
 	token: null,
 	name: null,
 	email: null,
+	profile: null,
+	profilePicture: null,
 };
 
 interface LoginPayload {
 	token: string;
 	name: string;
 	email: string;
+	profile: null;
+	profilePicture: null;
 }
 
 const authSlice = createSlice({
@@ -29,12 +35,16 @@ const authSlice = createSlice({
 			state.token = action.payload.token;
 			state.name = action.payload.name;
 			state.email = action.payload.email;
+			state.profile = action.payload.profile;
+			state.profilePicture = action.payload.profilePicture;
 		},
 		logout(state) {
 			state.isAuthenticated = false;
 			state.token = null;
 			state.name = null;
 			state.email = null;
+			state.profile = null;
+			state.profilePicture = null;
 		},
 	},
 });
