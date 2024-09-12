a277670119d8cab87ec063749404e18091f44973
Guilherme Augusto Borges Casagrande <38353118+GuilhermeMAG@users.noreply.github.com>
2024-09-11 21:48:24 -0300

    feat: integrate GraphQL query into PendingVouchers component
    - Added useQuery hook to fetch pending approval freight data using GET_PENDING_APPROVAL_FREIGHTS query.
    - Updated the component to dynamically render rows of pending vouchers for approval.

--- src/components/PendingVouchers/index.tsx
+++ src/components/PendingVouchers/index.tsx
@@ -1,32 +1,41 @@
-import React from "react";
-import styles from './PendingVouchers.module.css'
-import RowPendingVouchers from "./RowPendingVouchers";
-import { ArrowRightIcon } from "@/utils/icons";
+import { useQuery } from '@apollo/client';
+import { GET_PENDING_APPROVAL_FREIGHTS } from '@/graphql/queries/graphQueries';
+import RowPendingVouchers from './RowPendingVouchers';
+import { ArrowRightIcon } from '@/utils/icons';
+import styles from './PendingVouchers.module.css';
 
 const PendingVouchers = () => {
+  const { data, loading, error } = useQuery(GET_PENDING_APPROVAL_FREIGHTS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const pendingFreights = data?.getPendingApprovalFreights || [];
 
   const handleClick = () => {
-    console.log("Clicou em ver todos")
-  }
+    console.log('Clicou em ver todos');
+  };
 
   return (
     <div className={styles.container}>
       <p className={styles.title}>Comprovantes de entrega para aprovar</p>
 
-      <RowPendingVouchers numberOfPhotos="5" freightCode="111111" cte="1234" driverName="Mário ABC da Siva" />
-      <RowPendingVouchers numberOfPhotos="5" freightCode="111111" cte="1234" driverName="Mário ABC da Siva" />
-      <RowPendingVouchers numberOfPhotos="5" freightCode="111111" cte="1234" driverName="Mário ABC da Siva" />
-      <RowPendingVouchers numberOfPhotos="5" freightCode="111111" cte="1234" driverName="Mário ABC da Siva" />
-      <RowPendingVouchers numberOfPhotos="5" freightCode="111111" cte="1234" driverName="Mário ABC da Siva" />
+      {pendingFreights.map((freight: any) => (
+        <RowPendingVouchers
+          key={freight.freightCode}
+          numberOfPhotos={freight.photosCount}
+          freightCode={freight.freightCode}
+          cte={freight.numCte}
+          driverName={freight.driver}
+        />
+      ))}
 
       <div className={styles.buttonContainer} onClick={handleClick}>
         <p>Ver todos</p>
         <ArrowRightIcon />
       </div>
-
-      
     </div>
-  )
-}
+  );
+};
 
-export default PendingVouchers
+export default PendingVouchers;
