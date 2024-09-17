aaa19ec0478e34d4ed94230c35006b046a875a9b
Guilherme Augusto Borges Casagrande <38353118+GuilhermeMAG@users.noreply.github.com>
2024-09-17 15:40:40 -0300

    fixed freight type enum

--- src/utils/enums/typeEnum.ts
+++ src/utils/enums/typeEnum.ts
@@ -1,9 +1,4 @@
 export enum Type {
-<<<<<<< HEAD
   OFFER = "OFFER",
   TARGETED = "TARGETED",
-=======
-  OFERTA = "Oferta",
-  TARGETED = "Targeted",
->>>>>>> 9e03281 (feat(CreateFreight): added enums and update types)
 }
