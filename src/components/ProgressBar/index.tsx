95e84609e15870ddc2f854bf47274491e3375d5b
João Pedro <joaopedron253@gmail.com>
2024-09-23 13:04:39 -0300

    feat(icons on ProgressBar component): add freight stage icons to the ProgressBar component and create logic for changing colors according to the stage

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -1,6 +1,6 @@
-
 import React from 'react';
 import styles from './ProgressBar.module.css';
+import { ArrivalAtDestinationIcon, BalanceIcon, CarryingLoadIcon, InRouteIcon, PickupOrderIcon, SendingReceiptsIcon } from '@/utils/icons';
 
 interface ProgressBarProps {
   currentStage: number;
@@ -18,13 +18,26 @@ const ProgressBar: React.FC<ProgressBarProps> = ({ currentStage }) => {
 
   return (
     <div className={styles.progressBarContainer}>
+      <div className={styles.iconsContainer}>
+        {stages.map((stage, index) => (
+          <div key={index} className={styles.iconContainer} style={index === 3 ? { width: '92px' } : {}}>
+            {index === 0 && <PickupOrderIcon fill={currentStage >= 0 ? '#1C952C' : '#5D5A5A'} />}
+            {index === 1 && <CarryingLoadIcon fill={currentStage >= 1 ? '#1C952C' : '#5D5A5A'} />}
+            {index === 2 && <InRouteIcon fill={currentStage >= 2 ? '#1C952C' : '#5D5A5A'} />}
+            {index === 3 && <ArrivalAtDestinationIcon fill={currentStage >= 3 ? '#1C952C' : '#5D5A5A'} stroke={currentStage >= 3 ? '#1C952C' : '#5D5A5A'} />}
+            {index === 4 && <SendingReceiptsIcon fill={currentStage >= 4 ? '#1C952C' : '#5D5A5A'} />}
+            {index === 5 && <BalanceIcon fill={currentStage >= 5 ? '#1C952C' : '#5D5A5A'} />}
+            <p className={index <= currentStage ? styles.activeText : ''}>{stage.toUpperCase()}</p>
+          </div>
+        ))}
+      </div>
       <div className={styles.progressBar}>
         {stages.map((stage, index) => (
           <div
             key={index}
-            className={`${styles.stageDot} ${index === currentStage ? styles.activeDot : ''}`}
-            style={{ right: `${(index / (stages.length - 1)) *95}%` }}
-          ></div>
+            className={`${styles.stageDot} ${index < currentStage ? styles.prevDot : ''} ${index === currentStage ? styles.activeDot : ''}`}
+          >
+          </div>
         ))}
       </div>
     </div>
