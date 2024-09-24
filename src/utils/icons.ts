22e8c69a701d321e6939dcb9a5150fc16d0e384d
João Pedro <joaopedron253@gmail.com>
2024-09-24 11:28:33 -0300

    feat(DriverApproval page): create DriverrApproval page (index.tsx) and the initial structure

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -34,6 +34,7 @@ import InRoute from "@/assets/icons/inRoute.svg"
 import ArrivalAtDestination from "@/assets/icons/arrivalAtDestination.svg"
 import SendingReceipts from "@/assets/icons/sendingReceipts.svg"
 import Balance from "@/assets/icons/balance.svg"
+import Back from "@/assets/icons/back.svg"
 
 export const HomeIcon = Home;
 export const TruckIcon = Truck;
@@ -71,3 +72,4 @@ export const InRouteIcon = InRoute
 export const ArrivalAtDestinationIcon = ArrivalAtDestination
 export const SendingReceiptsIcon = SendingReceipts
 export const BalanceIcon = Balance
+export const BackIcon = Back
