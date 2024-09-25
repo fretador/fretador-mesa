fb1a361dfc347ee9e84de6835a409b910f724555
Rodrigo Alexandre <rodrigo.anst@gmail.com>
2024-09-25 09:31:27 -0300

    feature(LocationMap):added correctly key on map component

--- src/components/LocationMap/index.tsx
+++ src/components/LocationMap/index.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
-import styles from './LocationMap.module.css';
+import React from "react";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import styles from "./LocationMap.module.css";
 
 const containerStyle = {
-  width: '100%',
-  height: '400px'
+  width: "100%",
+  height: "400px",
 };
 
 const center = {
   lat: -3.745,
-  lng: -38.523
+  lng: -38.523,
 };
 
 const LocationMap = () => {
   return (
-    <LoadScript googleMapsApiKey="AIzaSyBtNNarnAAqG4almAYbKLIkIZVV">
+    <LoadScript googleMapsApiKey="AIzaSyDJMiTjF5Ihj0IJLGtVKycm88B0RUgwoHE">
       <div className={styles.mapContainer}>
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={10}
-        >
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
           {/* Você pode adicionar marcadores, polígonos, etc. aqui */}
         </GoogleMap>
       </div>
