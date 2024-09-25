c78c5ee34bc54cc3b8ad7ebd08be4a897cdf4975
Andrews2kx <andrews2k5@gmail.com>
2024-09-25 07:50:47 -0300

    feat: add LocationMap component

--- src/pages/frete-em-curso/index.tsx
+++ src/pages/frete-em-curso/index.tsx
@@ -13,81 +13,24 @@ import { SeparatorIcon } from "@/utils/icons";
 import ProgressBar from "@/components/ProgressBar";
 import FreightInCurseOptions from "@/components/FreightInCurseOptions";
 import FreightStep from "@/components/FreightStep";
-import { FreightService } from "@/services/freightService";
-import Loading from "@/components/Loading";
-import { FreightStatus } from "@/utils/enums/freightStatusEnum";
-import { Freight } from "@/utils/types/Freight";
+import LocationMap from "@/components/LocationMap"; // Adicione esta linha
 
-interface FreightInProgressProps {
-  freightId: string;
-}
-
-const FreightInProgress: React.FC<FreightInProgressProps> = ({ freightId }) => {
+const FreightInProgress: React.FC = () => {
   const isRetracted = useAppSelector((state) => state.sidebar.isRetracted);
   const router = useRouter();
-  const [freight, setFreight] = useState<Freight | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-  const [currentStage, setCurrentStage] = useState(0);
 
+  const routeName = router.pathname.replace("/", "").replaceAll("-", " ").toUpperCase();
+
+  // Definir o estado do estágio atual do frete
+  const [currentStage, setCurrentStage] = useState(0); // Inicializa com o estágio 0
+
+  // Simular a atualização do estágio atual do frete com dados mockados
   useEffect(() => {
-    const fetchFreightData = async () => {
-      try {
-        setLoading(true);
-        const freightData = await FreightService.getFreightById(freightId);
-        setFreight(freightData);
-        console.log("freightData", freightData);
-        setCurrentStage(
-          getStageFromStatus(freightData?.status as FreightStatus)
-        );
-      } catch (err) {
-        setError("Erro ao carregar os dados do frete");
-        console.error("Erro ao buscar dados do frete:", err);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    if (freightId) {
-      fetchFreightData();
-    }
-  }, [freightId]);
-
-  const getStageFromStatus = (status: FreightStatus): number => {
-    // Implemente a lógica para mapear o status do frete para um estágio
-    // Exemplo simplificado:
-    const stageMap: { [key in FreightStatus]?: number } = {
-      [FreightStatus.WAITING]: 0,
-      [FreightStatus.TARGETED]: 0,
-      [FreightStatus.REQUESTED]: 0,
-      [FreightStatus.APPROVED]: 0,
-      [FreightStatus.ACCEPTED]: 0,
-      [FreightStatus.DRIVER_SELECTED]: 0,
-      [FreightStatus.OPERATION_REQUIRED]: 0,
-      [FreightStatus.OPERATION_APPROVED]: 4,
-      [FreightStatus.ADMIN_REQUIRED]: 4,
-      [FreightStatus.ADMIN_APPROVED]: 4,
-      [FreightStatus.FINANCIAL_REQUIRED]: 5,
-      [FreightStatus.FINANCIAL_APPROVED]: 5,
-      [FreightStatus.LOADING_STARTED]: 2,
-      [FreightStatus.LOADING_FINISHED]: 2,
-      [FreightStatus.UNLOADING_STARTED]: 2,
-      [FreightStatus.UNLOADING_FINISHED]: 3,
-      [FreightStatus.INVOICE_SENT]: 4,
-      [FreightStatus.INVOICE_COUPON_SENT]: 4,
-      [FreightStatus.INVOICE_COUPON_REFUSED]: 4,
-      [FreightStatus.DRIVER_ARRIVED]: 5,
-    };
-    return stageMap[status] || 0;
-  };
-
-  if (loading) {
-    return <Loading />;
-  }
-
-  if (error) {
-    return <div>Erro: {error}</div>;
-  }
+    // Simula a atualização do estágio após 2 segundos
+    setTimeout(() => {
+      setCurrentStage(3); // Atualiza para o estágio 3 (Em rota)
+    }, 2000);
+  }, []);
 
   return (
     <AuthenticatedLayout>
@@ -102,86 +45,77 @@ const FreightInProgress: React.FC<FreightInProgressProps> = ({ freightId }) => {
           }
         >
           <div className={styles.header}>
-            <Header title={`FRETE EM CURSO - ${freight?.freightCode || ""}`} />
+            <Header title={routeName} />
           </div>
           <div className={styles.content}>
             <Body>
               <div>
-                <SearchComponent
-                  onSearch={(query) => console.log("Busca:", query)}
-                />
+                <SearchComponent />
               </div>
 
-              {freight && (
-                <div className={styles.freightInCurseContainer}>
-                  <FreightInCurseHeader
-                    freightCode={freight.freightCode.toString()}
-                    statusFreight={freight.status as FreightStatus}
-                    driverName={freight.targetedDrivers[0]?.name}
-                    origin={freight.origin}
-                    destination={freight.destination}
-                    driverPhoto={
-                      freight.targetedDrivers[0]?.userPhoto.imageUrl ||
-                      freight.targetedDrivers[0]?.userPhoto ||
-                      ""
-                    }
-                  />
+              <div className={styles.freightInCurseContainer}>
+                <FreightInCurseHeader
+                  freightCode="14123"
+                  statusFreight="em trânsito"
+                  driverName="Gilberto Marcha Lenta"
+                  cityOrigin="São Paulo"
+                  stateOrigin="SP"
+                  cityDestiny="Itarema"
+                  stateDestiny="CE"
+                />
 
-                  <SeparatorIcon />
+                <SeparatorIcon />
 
-                  <ProgressBar currentStage={currentStage} />
+                {/* Adicionar a barra de progresso aqui */}
+                <ProgressBar currentStage={currentStage} />
 
-                  <SeparatorIcon />
+                <SeparatorIcon />
 
-                  <div className={styles.freightInCurseOptionsContainer}>
-                    <h2>Dados do embarque:</h2>
-                    <FreightInCurseOptions />
-                  </div>
+                <div className={styles.freightInCurseOptionsContainer}>
+                  <h2>Dados do embarque:</h2>
+                  <FreightInCurseOptions />
                 </div>
-              )}
+              </div>
 
               <FreightStep
                 theme="dark"
-                date={freight?.updateDate}
-                content={
-                  freight?.status === FreightStatus.TARGETED
-                    ? "Frete enviado ao motorista"
-                    : "Frete solicitado pelo motorista"
-                }
+                date="07/06/2024"
+                hour="15:02:23"
+                content="Frete solicitado pelo motorista"
               />
 
               <FreightStep
                 theme="light"
                 date="07/06/2024"
-                content={
-                  freight?.status === FreightStatus.ACCEPTED
-                    ? "Frete aceito pelo motorista"
-                    : "Autorizar embarque"
-                }
+                hour="15:02:23"
+                content="Autorizar embarque?"
                 authorizeBoarding={true}
               />
 
               <FreightStep
                 theme="dark"
                 date="07/06/2024"
+                hour="15:02:23"
                 content="Motorista iniciou a viagem"
                 actionButton={true}
                 actionButtonText="rastrear"
-                handleActionButton={() => console.log("Botão de ação clicado")}
+                handleActionButton={() => console.log('Botão de ação clicado')}
               />
 
               <FreightStep
                 theme="light"
                 date="07/06/2024"
+                hour="15:02:23"
                 content="Anexo enviado pelo motorista"
                 actionButton={true}
                 actionButtonText="ver anexos"
-                handleActionButton={() => console.log("Botão de ação clicado")}
+                handleActionButton={() => console.log('Botão de ação clicado')}
                 hasAttachment={true}
-                attachmentPath={
-                  freight?.attachmentPath || "/default-attachment.png"
-                }
+                attachmentPath="/driver-mock.png"
               />
+
+              {/* Adicionar o componente LocationMap aqui */}
+              <LocationMap />
             </Body>
           </div>
         </div>
