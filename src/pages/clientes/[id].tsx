eea897c1505179c0ac371305a5e83a78b28227c6
João Pedro <joaopedron253@gmail.com>
2024-10-24 09:37:55 -0300

    feat: dynamic page creation and redirection for customer cards

---
+++ src/pages/clientes/[id].tsx
@@ -0,0 +1,149 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import Loading from '../../components/Loading';
+import styles from './Clientes.module.css';
+import AuthenticatedLayout from '@/components/AuthenticatedLayout';
+import Sidebar from '@/components/Sidebar';
+import Header from '@/components/Header';
+import Body from '@/components/Body';
+import { useAppSelector } from '@/store/store';
+import Botao from '@/components/Botao';
+import { BackIcon, LogoWhatsAppIcon, PencilSolidIcon, TrashIcon } from '@/utils/icons';
+import { mockClients } from '.';
+
+interface Client {
+  id: string,
+  cnpj: string,
+  corporateName: string,
+  tradeName: string,
+  city: string,
+  state: string,
+  email: string,
+  whatsapp: string,
+  stateRegistration: string,
+  address: string,
+  numberAddress: string,
+  neighborhood: string
+}
+
+const ServiceDetails = () => {
+  const isRetracted = useAppSelector((state) => state.sidebar.isRetracted);
+  const router = useRouter();
+  const { id } = router.query;
+  const [client, setClient] = useState<Client | null>(null)
+  const routeName = `Cliente #${id}`;
+
+  const backButtonContent = (
+    <div style={{ display: "flex", gap: "8px", alignItems: "center" }}>
+      <BackIcon /> <p style={{ fontWeight: "700" }}>Voltar</p>
+    </div>
+  );
+
+  const handleGoBack = () => {
+    router.back();
+  };
+
+  useEffect(() => {
+    if (id) {
+      const foundClient = mockClients.find(
+        (cli) => cli.id === id
+      );
+      setClient(foundClient || null);
+    }
+  }, [id]);
+
+  if (!client) {
+    return <p>Carregando...</p>;
+  }
+
+  return (
+    <AuthenticatedLayout>
+      <div className={styles.container}>
+        <div>
+          <Sidebar />
+        </div>
+
+        <div
+          className={
+            isRetracted ? styles.retractedContentWrapper : styles.contentWrapper
+          }
+        >
+          <div className={styles.header}>
+            <Header title={routeName} />
+          </div>
+          <div className={styles.content}>
+            <Body>
+              <div className={styles.backButtonContainer}>
+                <Botao text={backButtonContent} className={styles.backButton} onClick={handleGoBack} />
+              </div>
+
+              <div className={styles.serviceDetailsContainer}>
+                <div className={styles.informations}>
+                  <div className={styles.title}>
+                    <p>Dados do Cliente</p>
+                    <PencilSolidIcon />
+                  </div>
+
+                  <div className={styles.longRow}>
+                    <p>Nome: <span>{client.corporateName}</span></p>
+                    <p>CNPJ: <span>{client.cnpj}</span></p>
+                  </div>
+
+                  <div className={styles.row}>
+                    <p>Fantasia: <span>{client.tradeName}</span></p>
+                    <div className={styles.whatsappContainer}>
+                      <LogoWhatsAppIcon />
+                      <p>
+                        <span>{client.whatsapp}</span>
+                      </p>
+                    </div>
+                    <p>E-mail: <span>{client.email}</span></p>
+                  </div>
+
+                  <div className={styles.row}>
+                    <p>Inscrição Estadual: <span>{client.stateRegistration}</span></p>
+                    <p>Endereço: <span>{client.address}</span></p>
+                    <p>Número: <span>{client.numberAddress}</span></p>
+                    <p>Bairro: <span>{client.neighborhood}</span></p>
+                  </div>
+
+                  <div className={styles.row}>
+                    <p>Cidade: <span>{client.city}</span></p>
+                    <p>Estado: <span>{client.state}</span></p>
+                  </div>
+
+                  <div className={styles.title} style={{marginTop: '76px', marginBottom: '44px', cursor: 'pointer'}}>
+                    <p>Ver últimos embarques</p>
+                  </div>
+
+                  <div className={styles.row}>
+                    <p><span>Observações</span></p>
+                  </div>
+                  
+                  <div className={styles.textContainer}>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis eius, exercitationem iure autem nam quos natus distinctio omnis dignissimos quasi quis asperiores nostrum labore maxime libero sit. Deleniti, voluptas blanditiis!</p>
+                  </div>
+
+                  <div className={styles.actionButtonsContainer}>
+                    <Botao text="Salvar" onClick={() => console.log('Salvou')} className={styles.btnDark} />
+                  </div>
+
+                  <div className={styles.footer}>
+                    <p className={styles.dateAndTimeChangeText}>Última alteração feita por: Fulano ABC - 01/04/2024 às 10:44</p>
+                    <div className={styles.deleteContainer}>
+                      <p>Excluir Cadastro</p>
+                      <TrashIcon />
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </Body>
+          </div>
+        </div>
+      </div>
+    </AuthenticatedLayout>
+  )
+}
+
+export default ServiceDetails;
