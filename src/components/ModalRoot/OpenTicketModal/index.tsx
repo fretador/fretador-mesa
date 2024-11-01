2da87aa544c33f2be2a67cc42a6fde4704f9379f
Andrews2kx <andrews2k5@gmail.com>
2024-11-01 13:40:52 -0300

    feat: added ticket opening modal with selection, send and close functionality

---
+++ src/components/ModalRoot/OpenTicketModal/index.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import styles from './OpenTicketModal.module.css';
+
+interface OpenTicketModalProps {
+  onAdvance: () => void;
+  onRequestClose: () => void; // Adicionar a propriedade onRequestClose
+}
+
+const OpenTicketModal: React.FC<OpenTicketModalProps> = ({ onAdvance, onRequestClose }) => {
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedOption(event.target.value);
+  };
+
+  const handleSelectFocus = (event: React.FocusEvent<HTMLSelectElement>) => {
+    const selectElement = event.target;
+    if (selectElement.value === '') {
+      selectElement.value = ''; // Reset the value to ensure the placeholder is shown
+    }
+  };
+
+  const handleSelectBlur = (event: React.FocusEvent<HTMLSelectElement>) => {
+    const selectElement = event.target;
+    if (!selectedOption) {
+      selectElement.value = ''; // Reset the value to ensure the placeholder is shown
+    }
+  };
+
+  const handleAdvanceClick = () => {
+    if (selectedOption) {
+      onAdvance();
+    } else {
+      alert('Por favor, selecione uma opção de ticket.');
+    }
+  };
+
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.content}>
+        <div className={styles.modalHeader}>
+          <h2 className={styles.modalTitle}>Abrir Ticket</h2>
+          <button className={styles.closeButton} onClick={onRequestClose}>
+            X
+          </button>
+        </div>
+        <p className={styles.modalMessage}>Selecione a opção de ticket que deseja abrir:</p>
+        <select
+          className={styles.select}
+          value={selectedOption}
+          onChange={handleSelectChange}
+          onFocus={handleSelectFocus}
+          onBlur={handleSelectBlur}
+        >
+          <option value="" disabled hidden>Selecione</option>
+          <option value="mauFuncionamento">Mau Funcionamento</option>
+          <option value="duvidas">Dúvidas</option>
+        </select>
+        <button className={styles.button} onClick={handleAdvanceClick}>
+          Avançar
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default OpenTicketModal;
