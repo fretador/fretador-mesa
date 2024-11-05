245ec9de329fdfd6a5f6b53f9fcc17738031e5fc
Guilherme Augusto Borges Casagrande <38353118+GuilhermeMAG@users.noreply.github.com>
2024-11-05 09:12:55 -0300

    feat(Financial): added payment date

--- src/graphql/mutations/statusFreightMutation.ts
+++ src/graphql/mutations/statusFreightMutation.ts
@@ -1,19 +1,21 @@
 import { gql } from "@apollo/client";
 
 export const UPDATE_FREIGHT_STATUS = gql`
-  mutation UpdateFreightStatus(
-    $id: ID!
-    $status: FreightStatus!
-    $updateData: JSON!
-    $updateDataType: UpdateDataTypeEnum!
-  ) {
-    updateFreightStatus(
-      id: $id
-      status: $status
-      updateData: $updateData
-      updateDataType: $updateDataType
-    ) {
-      status
-    }
-  }
+	mutation UpdateFreightStatus(
+		$id: ID!
+		$status: FreightStatus!
+		$updateData: JSON!
+		$updateDataType: UpdateDataTypeEnum!
+	) {
+		updateFreightStatus(
+			id: $id
+			status: $status
+			updateData: $updateData
+			updateDataType: $updateDataType
+		) {
+			status
+			paymentDate
+		}
+	}
 `;
+
