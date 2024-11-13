ceb0c37547390bf6663da137852dac04676243ff
Guilherme Augusto Borges Casagrande <38353118+GuilhermeMAG@users.noreply.github.com>
2024-11-13 11:33:20 -0300

    feat: removed mocks and list real occurrence data

--- src/components/AnsweredOccurrencesList/index.tsx
+++ src/components/AnsweredOccurrencesList/index.tsx
@@ -1,214 +1,23 @@
+// src/components/AnsweredOccurrencesList.tsx
 import React from "react";
 import styles from "./AnsweredOccurrencesList.module.css";
 import { useRouter } from "next/router";
 import Loading from "../Loading";
 import RowTitle from "../RowTitle";
 import { Row } from "../Row";
+import { Occurrence } from "@/utils/Interfaces/Occurrence";
+import { OccurrenceStatus } from "@/utils/enums/occurrenceStatusEnum"
+import { occurrenceStatusLabels } from "@/utils/labels/occurrenceStatusLabels";
+import { occurrenceTypeLabels } from "@/utils/labels/occurrenceTypeLabels";
+import OccurrenceType from "../Row/OccurrenceType";
 
 interface AnsweredOccurrencesListProps {
   loading: boolean;
   error: string | null;
+  occurrences: Occurrence[];
 }
 
-export const mockOccurrences = [
-  {
-    id: '1',
-    driverName: "Carlos Silva",
-    freightNumber: "FR123456",
-    occurrenceType: "Atraso na entrega",
-    freightDate: "01/10/2024",
-    driverPhotoUrl: "/driver-mock.png",
-    route: "RJ x SP",
-    cte: '11111111',
-    attachments: ["/driver-mock.png", "/driver-mock.png", "/driver-mock.png"],
-    observations: "texto",
-    occurrenceStatus: "pendente",
-  },
-  {
-    id: '2',
-    driverName: "Maria Oliveira",
-    freightNumber: "FR123457",
-    occurrenceType: "Veículo quebrado",
-    freightDate: "02/10/2024",
-    driverPhotoUrl: "/driver-mock.png",
-    route: "SP x BH",
-    cte: '11111111',
-    attachments: ["/driver-mock.png", "/driver-mock.png", "/driver-mock.png"],
-    observations: "audio",
-    occurrenceStatus: "pendente",
-  },
-  {
-    id: '3',
-    driverName: "João Pedro",
-    freightNumber: "FR123458",
-    occurrenceType: "Carga danificada",
-    freightDate: "03/10/2024",
-    driverPhotoUrl: "/driver-mock.png",
-    route: "BH x RJ",
-    cte: '11111111',
-    attachments: ["/driver-mock.png", "/driver-mock.png", "/driver-mock.png"],
-    observations: "texto",
-    occurrenceStatus: "pendente",
-  },
-  {
-    id: '4',
-    driverName: "Ana Costa",
-    freightNumber: "FR123459",
-    occurrenceType: "Documentos incorretos",
-    freightDate: "04/10/2024",
-    driverPhotoUrl: "/driver-mock.png",
-    route: "SP x RJ",
-    cte: '11111111',
-    attachments: ["/driver-mock.png", "/driver-mock.png", "/driver-mock.png"],
-    observations: "audio",
-    occurrenceStatus: "pendente",
-  },
-  {
-    id: '5',
-    driverName: "Rafael Souza",
-    freightNumber: "FR123460",
-    occurrenceType: "Demora na carga",
-    freightDate: "05/10/2024",
-    driverPhotoUrl: "/driver-mock.png",
-    route: "RJ x MG",
-    cte: '11111111',
-    attachments: ["/driver-mock.png", "/driver-mock.png", "/driver-mock.png"],
-    observations: "texto",
-    occurrenceStatus: "pendente",
-  },
-  {
-    id: '6',
-    driverName: "Lucas Fernandes",
-    freightNumber: "FR123461",
-    occurrenceType: "Problema de comunicação",
-    freightDate: "06/10/2024",
-    driverPhotoUrl: "/driver-mock.png",
-    route: "MG x SP",
-    cte: '11111111',
-    attachments: ["/driver-mock.png", "/driver-mock.png", "/driver-mock.png"],
-    observations: "audio",
-    occurrenceStatus: "pendente",
-  },
-  {
-    id: '7',
-    driverName: "Julia Andrade",
-    freightNumber: "FR123462",
-    occurrenceType: "Erro de rota",
-    freightDate: "07/10/2024",
-    driverPhotoUrl: "/driver-mock.png",
-    route: "SP x RJ",
-    cte: '11111111',
-    attachments: ["/driver-mock.png", "/driver-mock.png", "/driver-mock.png"],
-    observations: "texto",
-    occurrenceStatus: "pendente",
-  },
-  {
-    id: '8',
-    driverName: "Gabriel Lima",
-    freightNumber: "FR123463",
-    occurrenceType: "Carga incompleta",
-    freightDate: "08/10/2024",
-    driverPhotoUrl: "/driver-mock.png",
-    route: "RJ x SP",
-    cte: '11111111',
-    attachments: ["/driver-mock.png", "/driver-mock.png", "/driver-mock.png"],
-    observations: "texto",
-    occurrenceStatus: "respondido",
-  },
-  {
-    id: '9',
-    driverName: "Fernanda Ribeiro",
-    freightNumber: "FR123464",
-    occurrenceType: "Atraso na entrega",
-    freightDate: "09/10/2024",
-    driverPhotoUrl: "/driver-mock.png",
-    route: "SP x BH",
-    cte: '11111111',
-    attachments: ["/driver-mock.png", "/driver-mock.png", "/driver-mock.png"],
-    observations: "audio",
-    occurrenceStatus: "em aberto",
-  },
-  {
-    id: '10',
-    driverName: "Leonardo Martins",
-    freightNumber: "FR123465",
-    occurrenceType: "Veículo quebrado",
-    freightDate: "10/10/2024",
-    driverPhotoUrl: "/driver-mock.png",
-    route: "RJ x MG",
-    cte: '11111111',
-    attachments: ["/driver-mock.png", "/driver-mock.png", "/driver-mock.png"],
-    observations: "texto",
-    occurrenceStatus: "reaberto",
-  },
-  {
-    id: '11',
-    driverName: "Carla Gomes",
-    freightNumber: "FR123466",
-    occurrenceType: "Carga danificada",
-    freightDate: "11/10/2024",
-    driverPhotoUrl: "/driver-mock.png",
-    route: "SP x RJ",
-    cte: '11111111',
-    attachments: ["/driver-mock.png", "/driver-mock.png", "/driver-mock.png"],
-    observations: "audio",
-    occurrenceStatus: "finalizado",
-  },
-  {
-    id: '12',
-    driverName: "Pedro Alves",
-    freightNumber: "FR123467",
-    occurrenceType: "Demora na carga",
-    freightDate: "12/10/2024",
-    driverPhotoUrl: "/driver-mock.png",
-    route: "RJ x SP",
-    cte: '11111111',
-    attachments: ["/driver-mock.png", "/driver-mock.png", "/driver-mock.png"],
-    observations: "texto",
-    occurrenceStatus: "respondido",
-  },
-  {
-    id: '13',
-    driverName: "Camila Ferreira",
-    freightNumber: "FR123468",
-    occurrenceType: "Erro de rota",
-    freightDate: "13/10/2024",
-    driverPhotoUrl: "/driver-mock.png",
-    route: "SP x MG",
-    cte: '11111111',
-    attachments: ["/driver-mock.png", "/driver-mock.png", "/driver-mock.png"],
-    observations: "audio",
-    occurrenceStatus: "em aberto",
-  },
-  {
-    id: '14',
-    driverName: "Felipe Santos",
-    freightNumber: "FR123469",
-    occurrenceType: "Problema de comunicação",
-    freightDate: "14/10/2024",
-    driverPhotoUrl: "/driver-mock.png",
-    route: "MG x RJ",
-    cte: '11111111',
-    attachments: ["/driver-mock.png", "/driver-mock.png", "/driver-mock.png"],
-    observations: "texto",
-    occurrenceStatus: "reaberto",
-  },
-  {
-    id: '15',
-    driverName: "Bruna Mendes",
-    freightNumber: "FR123470",
-    occurrenceType: "Carga incompleta",
-    freightDate: "15/10/2024",
-    driverPhotoUrl: "/driver-mock.png",
-    route: "RJ x SP",
-    cte: '11111111',
-    attachments: ["/driver-mock.png", "/driver-mock.png", "/driver-mock.png"],
-    observations: "audio",
-    occurrenceStatus: "finalizado",
-  },
-];
-
-const AnsweredOccurrencesList = ({ loading, error }: AnsweredOccurrencesListProps) => {
+const AnsweredOccurrencesList: React.FC<AnsweredOccurrencesListProps> = ({ loading, error, occurrences }) => {
   const router = useRouter();
 
   if (loading)
@@ -217,11 +26,16 @@ const AnsweredOccurrencesList = ({ loading, error }: AnsweredOccurrencesListProp
         <Loading />
       </div>
     );
-  if (error) return <p>Erro ao carregar motoristas: {error}</p>;
+  if (error) return <p>Erro ao carregar ocorrências: {error}</p>;
 
   const handleOccurrenceClick = (id: string) => {
     router.push(`/ocorrencias/${id}`);
   };
+
+  // Filtrar ocorrências respondidas ou outros status que não "pendente"
+  const answeredOccurrences = occurrences.filter(
+    (occurrence) => occurrence.status !== OccurrenceStatus.UNRESOLVED && occurrence.status !== OccurrenceStatus.IN_PROGRESS
+  );
 
   return (
     <div className={styles.container}>
@@ -240,25 +54,23 @@ const AnsweredOccurrencesList = ({ loading, error }: AnsweredOccurrencesListProp
       />
 
       <div className={styles.content}>
-        {mockOccurrences
-        .filter((occurrence) => occurrence.occurrenceStatus !== "pendente")
-        .map((occurrence) => (
+        {answeredOccurrences.map((occurrence) => (
           <Row.Root
             key={occurrence.id}
             customBackgroundColor="#B2CEDA"
             onClick={() => handleOccurrenceClick(occurrence.id)}
           >
             <Row.Driver
-              driverPhotoUrl={occurrence.driverPhotoUrl}
-              driverName={occurrence.driverName}
+              driverPhotoUrl={"/driver-mock.png"}
+              driverName={"DriverName"}
               showImage={true}
               textColor="#1B556D"
               textFontWeight="700"
             />
-            <Row.FreightDate date={occurrence.freightDate} />
-            <Row.FreightCode code={occurrence.freightNumber} />
-            <Row.OccurrenceType occurrenceType={occurrence.occurrenceType} />
-            <Row.OccurrenceStatus occurrenceStatus={occurrence.occurrenceStatus} />
+            <Row.OccurrenceDate occurrenceDate={new Date(occurrence.creationDate).toLocaleDateString()} />
+            <Row.FreightCode code={occurrence.freightCode || ""} />
+            <Row.OccurrenceType occurrenceType={occurrenceTypeLabels[occurrence.type as keyof typeof OccurrenceType]} />
+            <Row.OccurrenceStatus occurrenceStatus={occurrenceStatusLabels[occurrence.status as keyof typeof OccurrenceStatus]} />
           </Row.Root>
         ))}
       </div>
--- src/components/PendingOccurrencesCard/index.tsx
+++ src/components/PendingOccurrencesCard/index.tsx
@@ -3,13 +3,13 @@ import styles from './PendingOccurrencesCard.module.css'
 
 interface PendingOccurrencesCardProps {
   driverName: string,
-  freightNumber: string,
+  freightCode: string,
   occurrenceType: string,
   date: string,
   handleNewOccurrence: () => void
 }
 
-const PendingOccurrencesCard = ({driverName, freightNumber, occurrenceType, date, handleNewOccurrence}: PendingOccurrencesCardProps) => {
+const PendingOccurrencesCard = ({ driverName, freightCode, occurrenceType, date, handleNewOccurrence}: PendingOccurrencesCardProps) => {
   return (
     <div className={styles.container} onClick={handleNewOccurrence}>
 
@@ -19,9 +19,9 @@ const PendingOccurrencesCard = ({driverName, freightNumber, occurrenceType, date
 
       <div className={styles.separator}></div>
 
-      <div className={styles.freightNumberContainer}>
+      <div className={styles.freightCodeContainer}>
         <p className={styles.title}>N. Frete:</p>
-        <p className={styles.value}>{freightNumber}</p>
+        <p className={styles.value}>{freightCode}</p>
       </div>
 
       <div className={styles.occurrenceTypeContainer}>
