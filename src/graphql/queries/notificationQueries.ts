ff8e726970ba17de1d1d4e6ef64e09e8c714765f
Guilherme Augusto Borges Casagrande <38353118+GuilhermeMAG@users.noreply.github.com>
2025-01-03 17:26:12 -0300

    feat: created new notifications queries

--- src/graphql/queries/notificationQueries.ts
+++ src/graphql/queries/notificationQueries.ts
@@ -11,3 +11,39 @@ export const GET_NOTIFICATION_COUNTERS = gql`
 		}
 	}
 `;
+
+export const GET_BOARDUSER_NOTIFICATIONS = gql`
+	query GetBoardUserNotifications(
+		$userId: String
+		$groupKey: String
+		$includeAcknowledged: Boolean
+		$entityType: String
+	) {
+		notifications(
+			userId: $userId
+			groupKey: $groupKey
+			includeAcknowledged: $includeAcknowledged
+			entityType: $entityType
+		) {
+			_id
+			entityType
+			entityId
+			type
+			createdBy
+			recipients {
+				userId
+				acknowledged
+				acknowledgedAt
+			}
+			createdAt
+		}
+	}
+`;
+
+export const GET_TOTAL_NOTIFICATIONS = gql`
+	query GetTotalNotifications($userId: String) {
+		notifications(userId: $userId) {
+			_id
+		}
+	}
+`;
