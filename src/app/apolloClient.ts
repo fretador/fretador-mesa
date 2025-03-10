7b61175fc1081a7654d505853171413d6c9a9da6
Guilherme Augusto Borges Casagrande <g.casagrande@uni9.edu.br>
2025-03-10 17:25:11 -0300

    build: updated server url

--- src/app/apolloClient.ts
+++ src/app/apolloClient.ts
@@ -3,7 +3,7 @@ import { setContext } from "@apollo/client/link/context";
 import { AuthService } from "@/services/authService";
 
 const httpLink = new HttpLink({
-	uri: `${process.env.NEXT_PUBLIC_API_URL}/graphql`,
+	uri: process.env.NEXT_PUBLIC_API_URL,
 });
 
 const authLink = setContext((_, { headers }) => {
