c6d5cbe407769be3b97dc99551d7827eb6de78b2
Guilherme Augusto Borges Casagrande <g.casagrande@uni9.edu.br>
2025-03-19 18:16:00 -0300

    feat: created graphql websocket flux

--- src/app/apolloClient.ts
+++ src/app/apolloClient.ts
@@ -1,10 +1,26 @@
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+// src/config/apollo.ts
+import { ApolloClient, InMemoryCache, HttpLink, split } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
 import { AuthService } from "@/services/authService";
 
 const httpLink = new HttpLink({
 	uri: process.env.NEXT_PUBLIC_API_URL,
 });
+
+const wsLink = new GraphQLWsLink(
+	createClient({
+		url: process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:4000/graphql",
+		connectionParams: () => {
+			const token = AuthService.getBoardUserToken();
+			return {
+				authorization: token ? `Bearer ${token}` : "",
+			};
+		},
+	})
+);
 
 const authLink = setContext((_, { headers }) => {
 	const token = AuthService.getBoardUserToken();
@@ -16,8 +32,20 @@ const authLink = setContext((_, { headers }) => {
 	};
 });
 
+const splitLink = split(
+	({ query }) => {
+		const definition = getMainDefinition(query);
+		return (
+			definition.kind === "OperationDefinition" &&
+			definition.operation === "subscription"
+		);
+	},
+	wsLink,
+	authLink.concat(httpLink)
+);
+
 const apolloClient = new ApolloClient({
-	link: authLink.concat(httpLink),
+	link: splitLink,
 	cache: new InMemoryCache(),
 });
 
